b71fbdf843908a374bd3439f2f62d323a144b80f
jose00796 <josebalboa11@hotmail.com>
2020-02-14 10:38:42 -0400

    creacion de una rama de Desarrollo Papu

--- index.php
+++ index.php
@@ -19,22 +19,11 @@ spl_autoload_register(function($classname){
 
 //VE SI TE PONES A TRABAJAR EN ESTA MIERDA COÑO E TU PEPA...
 
-$teacher = new profesor('Violet Starr', 25, 'F', 'Follar');
-$teacher2 = new profesor('Mia Malkova', 25, 'F', 'Matematica');
-$teacher3 = new profesor('ALL Might', 55, 'M', 'Programacion');
+$student = new ejecutable();
 
-$room = new aula('Follar');
+$ListS = $student->GetStudents();
 
-$student = new alumno('Jose', 23, 'M');
-
-$student->Assistance();
-$student->Assistance();
-$student->Assistance();
-$student->Assistance();
-$student->GetScore();
-$teacher->Approve($student);
-
-$room->TeacherAssistance($teacher);
-$room->StudentAssistance($student);//AUN FALTAN UNAS COSAS
-$room->CorrectTeacher($teacher);
-$room->OpenRoom();
+foreach ($ListS as $student) {
+    Show("{$student->GetName()}");
+    $student->Assistance();
+}

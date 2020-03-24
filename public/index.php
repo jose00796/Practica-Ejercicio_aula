a7ee37bcad7296b45722b5ac15e410cfc50c2802
jose00796 <josedarkrai@gmail.com>
2020-03-23 22:36:51 -0400

    Programa listo, se le agrego una autocarga con Composer, ya no le quiero hacer mas nada a menos que aprenda algo nuevo, 20pts pal Pecho Carajo :)

--- public/index.php
+++ public/index.php
@@ -4,17 +4,7 @@ namespace Curso;
 
 use Exception;
 
-require "src/helpers.php";
-spl_autoload_register(function($classname){
-       
-    if (strpos($classname, 'Curso\\') === 0) {
-            $classname = str_replace('Curso\\','',$classname);
-
-            if (file_exists("src/$classname.php")) {
-                require "src/$classname.php";
-            }
-        }
-});
+require '../vendor/autoload.php';
 
 //30/01/2020 ME PARECE UNA BUENA IDEA PASAR EL PROFESOR COMO UNA DEPENDENCIA MEDIANTE EL CONSTRUCTOR DEL OBJETO ROOM
 //TRATA DE DESARROLLAR ESTA IDEA GUAPO.
